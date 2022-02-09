70e4ee10303c1699bb29c33f1559ed5ae0be8f9c
andreylara <andreydelara@hotmail.com>
2022-02-09 20:45:41 -0300

    Upload de imagem do produto

--- gopizza/src/screens/Product/index.tsx
+++ gopizza/src/screens/Product/index.tsx
@@ -4,7 +4,13 @@ import { Platform, TouchableOpacity } from "react-native";
 import { ButtonBack } from "@components/ButtonBack";
 import { Photo } from "@components/Photo";
 
-import { Container, Header, Title, DeleteLabel } from "./styles";
+import { 
+    Container, 
+    Header, 
+    Title, 
+    DeleteLabel,
+    Upload,
+    PickImageButton } from "./styles";
 
 export function Product() {
     return (
@@ -21,7 +27,10 @@ export function Product() {
                 
             </Header>
 
-            <Photo uri="https://github.com/andreylara.png" />
+            <Upload>
+                <Photo uri="https://github.com/andreylara.png" />
+                <PickImageButton title="Carregar" type="secondary"/>
+            </Upload>
         </Container>
     )
 }
