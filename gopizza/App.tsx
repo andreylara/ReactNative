2d45fd9b25819045836f0321904fe48080f39a4e
andreylara <andreydelara@hotmail.com>
2022-02-10 19:28:39 -0300

    Criação da tela inicial

--- gopizza/App.tsx
+++ gopizza/App.tsx
@@ -10,7 +10,7 @@ import { AuthProvider } from '@hooks/auth';
 import theme from './src/theme';
 
 //import { SignIn } from '@screens/SignIn';
-import { Product } from '@screens/Product';
+import { Home } from '@screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,7 +27,7 @@ export default function App() {
       <StatusBar style="light" translucent backgroundColor="transparent" />
       
       <AuthProvider>
-        <Product />
+        <Home />
       </AuthProvider>
     </ThemeProvider>
   );
