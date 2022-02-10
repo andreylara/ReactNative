752ef0282cb4292b42e3473b7760cd36508e943a
andreylara <andreydelara@hotmail.com>
2022-02-09 23:48:20 -0300

    Carregamento de imagem

--- gopizza/src/screens/Product/index.tsx
+++ gopizza/src/screens/Product/index.tsx
@@ -1,35 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import { Platform, TouchableOpacity } from "react-native";
+import * as ImagePicker from 'expo-image-picker';
 
 import { ButtonBack } from "@components/ButtonBack";
 import { Photo } from "@components/Photo";
 
-import { 
-    Container, 
-    Header, 
-    Title, 
+import {
+    Container,
+    Header,
+    Title,
     DeleteLabel,
     Upload,
-    PickImageButton } from "./styles";
+    PickImageButton
+} from "./styles";
 
 export function Product() {
+    const [image, setImage] = useState('');
+
+    async function handlePickerImage() {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status === 'granted') {
+            const result = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.Images,
+                aspect: [4, 4]
+            });
+
+            if (!result.cancelled) {
+                setImage(result.uri);
+            }
+        }
+    }
+
     return (
-        <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>            
+        <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
             <Header>
-                
+
                 <ButtonBack />
-                
+
                 <Title>Cadastrar</Title>
-                
+
                 <TouchableOpacity>
                     <DeleteLabel>Deletar</DeleteLabel>
                 </TouchableOpacity>
-                
+
             </Header>
 
             <Upload>
-                <Photo uri="https://github.com/andreylara.png" />
-                <PickImageButton title="Carregar" type="secondary"/>
+                <Photo uri={image} />
+                
+                <PickImageButton 
+                    title="Carregar" 
+                    type="secondary" 
+                    onPress={handlePickerImage}/>
             </Upload>
         </Container>
     )
