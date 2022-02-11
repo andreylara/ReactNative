2319adcb1011125960b9593a45320d30ad7a6223
andreylara <andreydelara@hotmail.com>
2022-02-10 21:29:12 -0300

    Listando produtos cadastrados

--- gopizza/src/screens/Home/index.tsx
+++ gopizza/src/screens/Home/index.tsx
@@ -1,11 +1,13 @@
-import React from "react";
-import { TouchableOpacity } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Alert, TouchableOpacity, FlatList } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { useTheme } from "styled-components";
+import firestore from '@react-native-firebase/firestore';
 
 import happyEmoji from '@assets/happy.png';
 
 import { Search } from "@components/Search";
+import { ProductCard, ProductProps } from "@components/ProductCard";
 
 import { 
     Container,
@@ -19,27 +21,72 @@ import {
 } from "./styles";
 
 export function Home() {
+    const [pizzas, setPizzas] = useState<ProductProps[]>([]);
     const { COLORS } = useTheme();
+
+    function fetchPizzas(value: string){
+        const formattedValue = value.toLocaleLowerCase().trim();
+
+        firestore()
+        .collection('pizzas')
+        .orderBy('name_insensitive')
+        .startAt(formattedValue)
+        .endAt(`${formattedValue}\uf8ff`)
+        .get()
+        .then(response => {
+            const data = response.docs.map(doc => {
+                return {
+                    id: doc.id,
+                    ...doc.data(),
+                }
+            }) as ProductProps[];
+
+            setPizzas(data);
+        })
+        .catch(() => Alert.alert('Consulta', 'Não foi possível realizar a consulta'));
+    }
+
+    useEffect(() => {
+        fetchPizzas('');
+    }, []);
 
     return(
         <Container>
             <Header>
                 <Greeting>
-                    <GreetingEmoji source={happyEmoji} />
+                    <GreetingEmoji 
+                        source={happyEmoji} />
                     <GreetingText>Olá, Admin</GreetingText>
                 </Greeting>
 
                 <TouchableOpacity>
-                    <MaterialIcons name="logout" color={COLORS.TITLE} size={24} />
+                    <MaterialIcons 
+                        name="logout" 
+                        color={COLORS.TITLE} 
+                        size={24} />
                 </TouchableOpacity>
             </Header>
 
-            <Search onSearch={() => { }} onClear={() => { }} />
+            <Search 
+                onSearch={() => { }} 
+                onClear={() => { }} />
 
             <MenuHeader>
                 <Title>Cardápio</Title>
                 <MenuItemsNumber>10 pizzas</MenuItemsNumber>
             </MenuHeader>
+
+            <FlatList 
+                data={pizzas}
+                keyExtractor={item => item.id}
+                renderItem={({ item }) => <ProductCard data={item}/>}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{
+                    paddingTop: 20,
+                    paddingBottom: 125,
+                    marginHorizontal: 24
+                }} />
+
         </Container>
     )
 }
